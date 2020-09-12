d1d56a50c9baffe7496381983f888568ac733fcd
Leah Schlackman <leahemily1@gmail.com>
2020-09-12 20:44:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,13 +5,13 @@ def key_for_min_value(name_hash)
   arr = name_hash.to_a
   d_value = 1000
   d_key = 0 
-  arr.each do |x|
+  if arr == []
+    puts "Empty hash."
+  else arr.each do |x|
     if x[1] < d_value
       d_value = x[1]
       d_key = x[0]
-      d_key
-    else 
-      puts "Empty hash"
     end
   end
+  d_key
 end
