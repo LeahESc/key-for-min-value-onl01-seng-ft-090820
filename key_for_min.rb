c744174b33c6e51744f57d17d9b39610aa52e9dc
Leah Schlackman <leahemily1@gmail.com>
2020-09-12 20:48:49 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,13 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  if 
-  arr = name_hash.to_a
-  d_value = 1000
-  d_key = 0 
-    if arr == 0
-     puts "nil."
-    else arr.each do |x|
+  if (name_hash).empty?
+    puts "Empty hash."
+  else 
+    arr = name_hash.to_a
+    d_value = 1000
+    d_key = 0 
+    arr.each do |x|
       if x[1] < d_value
       d_value = x[1]
       d_key = x[0]
