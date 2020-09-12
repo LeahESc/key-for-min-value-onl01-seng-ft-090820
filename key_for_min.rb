1c368bc46d99d0a04fcbe59a9bf9064625b393bb
Leah Schlackman <leahemily1@gmail.com>
2020-09-12 20:28:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  arr = name_hash.to_a
+  d_value = 1000
+  d_key = 0 
+  arr.each do |x|
+    if x < 
+  
+  each do |key, value|
+    if :value[1] < value[]
+  key
 end
